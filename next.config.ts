bb7b301e1999c8e1d19613e23f3bf9c925598f3f
Patolord <rodrigo_luizp@hotmail.com>
2025-06-29 15:21:07 -0300

    Remove security headers from Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,25 +2,6 @@ import { withSentryConfig } from '@sentry/nextjs';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-
-async headers() {
-  return [
-    {
-      source: "/:path*",
-      headers: [
-        {
-          key: 'Cross-Origin-Opener-Policy',
-          value: 'same-origin',
-        },
-        {
-          key: 'Cross-Origin-Embedder-Policy',
-          value: 'require-corp',
-        },
-      ],
-    },
-  ]
-},
-
   trailingSlash: false,
   // This is required to support PostHog trailing slash API requests
   skipTrailingSlashRedirect: true,
