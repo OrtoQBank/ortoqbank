dd44b8cca60c286e4254098351a9a2d46c407315
Rodrigo Luiz Schil Pellegrini <rodrigo_luizp@hotmail.com>
2025-09-29 21:28:13 -0300

    install gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 import './globals.css';
 
+import ErrorBoundary from '@/components/error-boundary';
+import { PostHogProvider } from '@/components/PostHogProvider';
+import { ErrorLogger } from '@/components/providers/error-logger';
+import { Toaster } from '@/components/ui/toaster';
+import { GoogleTagManager } from '@next/third-parties/google';
 import { Analytics } from '@vercel/analytics/next';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import localFont from 'next/font/local';
 import Script from 'next/script';
 import NextTopLoader from 'nextjs-toploader';
-
-import ErrorBoundary from '@/components/error-boundary';
-import { PostHogProvider } from '@/components/PostHogProvider';
-import { ErrorLogger } from '@/components/providers/error-logger';
-import { Toaster } from '@/components/ui/toaster';
 
 import ConvexClientProvider from './convex-client-provider';
 
@@ -65,6 +65,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <GoogleTagManager gtmId="GTM-K7CS8TFH" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${sifonn.variable} antialiased`}
       >
